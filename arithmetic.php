1ec237559705552addc2b506971fd236dd2ae2b2
phisit77 <takaob037@gmail.com>
2023-01-11 15:24:57 +0700

    update

---
+++ arithmetic.php
@@ -0,0 +1,25 @@
+<?php
+//Arithemitic Operator
+    $x = 10;
+    $y = 4;
+    echo "$x + $y = ",$x + $y, "<br>";
+
+    #1.Addition
+    echo "$x + $y = ", $x + $y, "<br>";
+
+    #2.Subtraction
+    echo "$x - $y = ", $x - $y, "<br>";
+
+    #3.Multiply
+    echo "$x * $y = ", $x * $y, "<br>";
+
+    #4.Division
+    echo "$x / $y = ", $x / $y, "<br>";
+
+    #5.Remaining
+    echo "$x % $y = ", $x % $y, "<br>";
+
+    #6.Exponential
+    echo "$x ** $y = ", $x ** $y, "<br>";
+
+    ?>
---
+++ assignment.php
@@ -0,0 +1,21 @@
+<?php
+    $x = 10;
+
+    #1.Add and Assignment value
+    echo "$x += 20 =",$x += 20, "<br>"; //x = x + 20 //
+
+    #2.Subtract Assignment value
+    echo "$x -= 20 =",$x -= 20, "<br>"; //x = x - 20 //
+
+    #3.Multiply Assignment value
+    echo "$x *= 20 =",$x *= 20, "<br>"; //x = x * 20 //
+
+    #4.Divisio Assignment value
+    echo "$x /= 20 =",$x /= 20, "<br>"; //x = x / 20 //
+
+    #5.Modulus Assignment value
+    echo "$x %= 20 =",$x %= 20, "<br>"; //x = x % 20 //
+
+    #6.Exponential Assignment value
+    echo "$x **= 20 =",$x **= 20, "<br>"; //x = x ** 20 //
+?>
